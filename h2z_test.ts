330e114ea15d3b7efe1bb532b8f8840b7ae29d54
諏訪原慶斗 <131525647+suwakei@users.noreply.github.com>
2025-02-26 15:14:15 +0900

    の更新

---
+++ h2z_test.ts
@@ -0,0 +1,26 @@
+import { z2h } from "./mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+Deno.test("z2h: 全角文字を半角に変換", () => {
+  const input = "Ｈｅｌｌｏ， ｗｏｒｌｄ！";
+  const expected = "Hello, world!";
+  assertEquals(z2h(input), expected);
+});
+
+Deno.test("z2h: 半角文字を含む全角文字列を半角に変換", () => {
+  const input = "Ｈｅｌｌｏ, world";
+  const expected = "Hello, world!";
+  assertEquals(z2h(input), expected);
+});
+
+Deno.test("z2h: 空文字列の処理", () => {
+  const input = "";
+  const expected = "";
+  assertEquals(z2h(input), expected);
+});
+
+Deno.test("z2h: nullの処理", () => {
+  const input = null;
+  const expected = "";
+  assertEquals(z2h(input), expected);
+});
