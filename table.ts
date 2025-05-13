88018812badbab9abcfaa0f3fd21f7be9c9d0ea1
keitosuwahara <keitosuwahara0816@gmail.com>
2025-05-13 15:28:15 +0900

    mod table

--- table.ts
+++ table.ts
@@ -1,33 +1,147 @@
+/**
+ * Full Width ASCII characters map.
+ */
+export const ASCII_H2Z_CHARS_MAP: Record<string, string> = {
+  'a': 'ａ', 'b': 'ｂ', 'c': 'ｃ', 'd': 'ｄ', 'e': 'ｅ', 'f': 'ｆ', 'g': 'ｇ', 'h': 'ｈ', 'i': 'ｉ', 'j': 'ｊ', 'k': 'ｋ', 'l': 'ｌ', 'm': 'ｍ', 'n': 'ｎ', 'o': 'ｏ', 'p': 'ｐ', 'q': 'ｑ', 'r': 'ｒ', 's': 'ｓ', 't': 'ｔ', 'u': 'ｕ', 'v': 'ｖ', 'w': 'ｗ', 'x': 'ｘ', 'y': 'ｙ', 'z': 'ｚ',
+  'A': 'Ａ', 'B': 'Ｂ', 'C': 'Ｃ', 'D': 'Ｄ', 'E': 'Ｅ', 'F': 'Ｆ', 'G': 'Ｇ', 'H': 'Ｈ', 'I': 'Ｉ', 'J': 'Ｊ', 'K': 'Ｋ', 'L': 'Ｌ', 'M': 'Ｍ', 'N': 'Ｎ', 'O': 'Ｏ', 'P': 'Ｐ', 'Q': 'Ｑ', 'R': 'Ｒ', 'S': 'Ｓ', 'T': 'Ｔ', 'U': 'Ｕ', 'V': 'Ｖ', 'W': 'Ｗ', 'X': 'Ｘ', 'Y': 'Ｙ', 'Z': 'Ｚ',
+  '!': '！', '"': '”', '#': '＃', '$': '＄', '%': '％', '&': '＆', '\'': '’', '(': '（', ')': '）', '*': '＊', '+': '＋', ',': '，', '-': '－', '.': '．', '/': '／', ':': '：', ';': '；', '<': '＜', '=': '＝', '>': '＞', '?': '？', '@': '＠', '[': '［', '¥': '￥', ']': '］', '^': '＾',
+  '_': '＿', '`': '‘', '{': '｛', '|': '｜', '}': '｝', '~': '～', ' ': '　', '\\': '＼',
+};
 
-export const ASCII_ZENKAKU_CHARS = [
-  'ａ', 'ｂ', 'ｃ', 'ｄ', 'ｅ', 'ｆ', 'ｇ', 'ｈ', 'ｉ', 'ｊ', 'ｋ', 'ｌ', 'ｍ', 'ｎ', 'ｏ', 'ｐ', 'ｑ', 'ｒ', 'ｓ', 'ｔ', 'ｕ', 'ｖ', 'ｗ', 'ｘ', 'ｙ', 'ｚ',
-  'Ａ', 'Ｂ', 'Ｃ', 'Ｄ', 'Ｅ', 'Ｆ', 'Ｇ', 'Ｈ', 'Ｉ', 'Ｊ', 'Ｋ', 'Ｌ', 'Ｍ', 'Ｎ', 'Ｏ', 'Ｐ', 'Ｑ', 'Ｒ', 'Ｓ', 'Ｔ', 'Ｕ', 'Ｖ', 'Ｗ', 'Ｘ', 'Ｙ', 'Ｚ',
-  '！', '”', '＃', '＄', '％', '＆', '’', '（', '）', '＊', '＋', '，', '－', '．', '／', '：', '；', '＜', '＝', '＞', '？', '＠', '［', '￥', '］', '＾', '＿', '‘', '｛', '｜', '｝', '～', '　', '＼'
-];
+/**
+ * Half Width ASCII characters map.
+ */
+export const ASCII_Z2H_CHARS_MAP: Record<string, string> = {
+  'ａ': 'a', 'ｂ': 'b', 'ｃ': 'c', 'ｄ': 'd', 'ｅ': 'e', 'ｆ': 'f', 'ｇ': 'g', 'ｈ': 'h', 'ｉ': 'i', 'ｊ': 'j', 'ｋ': 'k', 'ｌ': 'l', 'ｍ': 'm', 'ｎ': 'n', 'ｏ': 'o', 'ｐ': 'p', 'ｑ': 'q', 'ｒ': 'r', 'ｓ': 's', 'ｔ': 't', 'ｕ': 'u', 'ｖ': 'v', 'ｗ': 'w', 'ｘ': 'x', 'ｙ': 'y', 'ｚ': 'z',
+  'Ａ': 'A', 'Ｂ': 'B', 'Ｃ': 'C', 'Ｄ': 'D', 'Ｅ': 'E', 'Ｆ': 'F', 'Ｇ': 'G', 'Ｈ': 'H', 'Ｉ': 'I', 'Ｊ': 'J', 'Ｋ': 'K', 'Ｌ': 'L', 'Ｍ': 'M', 'Ｎ': 'N', 'Ｏ': 'O', 'Ｐ': 'P', 'Ｑ': 'Q', 'Ｒ': 'R', 'Ｓ': 'S', 'Ｔ': 'T', 'Ｕ': 'U', 'Ｖ': 'V', 'Ｗ': 'W', 'Ｘ': 'X', 'Ｙ': 'Y', 'Ｚ': 'Z',
+  '！': '!', '”': '"', '＃': '#', '＄': '$', '％': '%', '＆': '&', '’': '\'', '（': '(', '）': ')', '＊': '*', '＋': '+', '，': ',', '－': '-', '．': '.', '／': '/', '：': ':', '；': ';', '＜': '<', '＝': '=', '＞': '>', '？': '?', '＠': '@', '［': '[', '￥': '¥', '］': ']', '＾': '^',
+  '＿': '_', '‘': '`', '｛': '{', '｜': '|', '｝': '}', '～': '~', '　': ' ', '＼': '\\',
+};
 
-export const ASCII_HANKAKU_CHARS = [
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-  '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '¥', ']', '^', '_', '`', '{', '|', '}', '~', ' ', '\\'
-];
+/**
+ * Half Width KANA characters map.
+ */
+export const KANA_H2Z_CHARS_MAP: Record<string, string> = {
+  'ｱ': 'ア', 'ｲ': 'イ', 'ｳ': 'ウ', 'ｴ': 'エ', 'ｵ': 'オ',
+  'ｶ': 'カ', 'ｷ': 'キ', 'ｸ': 'ク', 'ｹ': 'ケ', 'ｺ': 'コ',
+  'ｻ': 'サ', 'ｼ': 'シ', 'ｽ': 'ス', 'ｾ': 'セ', 'ｿ': 'ソ',
+  'ﾀ': 'タ', 'ﾁ': 'チ', 'ﾂ': 'ツ', 'ﾃ': 'テ', 'ﾄ': 'ト',
+  'ﾅ': 'ナ', 'ﾆ': 'ニ', 'ﾇ': 'ヌ', 'ﾈ': 'ネ', 'ﾉ': 'ノ',
+  'ﾊ': 'ハ', 'ﾋ': 'ヒ', 'ﾌ': 'フ', 'ﾍ': 'ヘ', 'ﾎ': 'ホ',
+  'ﾏ': 'マ', 'ﾐ': 'ミ', 'ﾑ': 'ム', 'ﾒ': 'メ', 'ﾓ': 'モ',
+  'ﾔ': 'ヤ', 'ﾕ': 'ユ', 'ﾖ': 'ヨ',
+  'ﾗ': 'ラ', 'ﾘ': 'リ', 'ﾙ': 'ル', 'ﾚ': 'レ', 'ﾛ': 'ロ',
+  'ﾜ': 'ワ', 'ｦ': 'ヲ', 'ﾝ': 'ン',
+  'ｧ': 'ァ', 'ｨ': 'ィ', 'ｩ': 'ゥ', 'ｪ': 'ェ', 'ｫ': 'ォ',
+  'ｯ': 'ッ',
+  'ｬ': 'ャ', 'ｭ': 'ュ', 'ｮ': 'ョ',
+  '｡': '。', '､': '、', '･': '・', 'ﾞ': '゛', 'ﾟ': '゜',
+  '｢': '「', '｣': '」', 'ｰ': 'ー',
+};
 
-export const KANA_ZENKAKU_CHARS = [
-  'ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', 'ヲ', 'ン', 'ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ッ', 'ャ', 'ュ', 'ョ', '。', '、', '・', '゛', '゜', '「', '」', 'ー'
-];
+/**
+ * Full Width KANA characters map.
+ */
+export const KANA_Z2H_CHARS_MAP: Record<string, string> = {
+  'ア': 'ｱ', 'イ': 'ｲ', 'ウ': 'ｳ', 'エ': 'ｴ', 'オ': 'ｵ',
+  'カ': 'ｶ', 'キ': 'ｷ', 'ク': 'ｸ', 'ケ': 'ｹ', 'コ': 'ｺ',
+  'サ': 'ｻ', 'シ': 'ｼ', 'ス': 'ｽ', 'セ': 'ｾ', 'ソ': 'ｿ',
+  'タ': 'ﾀ', 'チ': 'ﾁ', 'ツ': 'ﾂ', 'テ': 'ﾃ', 'ト': 'ﾄ',
+  'ナ': 'ﾅ', 'ニ': 'ﾆ', 'ヌ': 'ﾇ', 'ネ': 'ﾈ', 'ノ': 'ﾉ',
+  'ハ': 'ﾊ', 'ヒ': 'ﾋ', 'フ': 'ﾌ', 'ヘ': 'ﾍ', 'ホ': 'ﾎ',
+  'マ': 'ﾏ', 'ミ': 'ﾐ', 'ム': 'ﾑ', 'メ': 'ﾒ', 'モ': 'ﾓ',
+  'ヤ': 'ﾔ', 'ユ': 'ﾕ', 'ヨ': 'ﾖ',
+  'ラ': 'ﾗ', 'リ': 'ﾘ', 'ル': 'ﾙ', 'レ': 'ﾚ', 'ロ': 'ﾛ',
+  'ワ': 'ﾜ', 'ヲ': 'ｦ', 'ン': 'ﾝ',
+  'ァ': 'ｧ', 'ィ': 'ｨ', 'ゥ': 'ｩ', 'ェ': 'ｪ', 'ォ': 'ｫ',
+  'ッ': 'ｯ',
+  'ャ': 'ｬ', 'ュ': 'ｭ', 'ョ': 'ｮ',
+  '。': '｡', '、': '､', '・': '･', '゛': 'ﾞ', '゜': 'ﾟ',
+  '「': '｢', '」': '｣', 'ー': 'ｰ',
+};
 
-export const KANA_HANKAKU_CHARS = [
-  'ｱ', 'ｲ', 'ｳ', 'ｴ', 'ｵ', 'ｶ', 'ｷ', 'ｸ', 'ｹ', 'ｺ', 'ｻ', 'ｼ', 'ｽ', 'ｾ', 'ｿ', 'ﾀ', 'ﾁ', 'ﾂ', 'ﾃ', 'ﾄ', 'ﾅ', 'ﾆ', 'ﾇ', 'ﾈ', 'ﾉ', 'ﾊ', 'ﾋ', 'ﾌ', 'ﾍ', 'ﾎ', 'ﾏ', 'ﾐ', 'ﾑ', 'ﾒ', 'ﾓ', 'ﾔ', 'ﾕ', 'ﾖ', 'ﾗ', 'ﾘ', 'ﾙ', 'ﾚ', 'ﾛ', 'ﾜ', 'ｦ', 'ﾝ', 'ｧ', 'ｨ', 'ｩ', 'ｪ', 'ｫ', 'ｯ', 'ｬ', 'ｭ', 'ｮ', '｡', '､', '･', 'ﾞ', 'ﾟ', '｢', '｣', 'ｰ'
-];
+/**
+ * Half Width number characters map.
+ */
+export const DIGIT_H2Z_CHARS_MAP: Record<string, string> = {
+  '0': '０', '1': '１', '2': '２', '3': '３', '4': '４',
+  '5': '５', '6': '６', '7': '７', '8': '８', '9': '９',
+};
 
-export const DIGIT_ZENKAKU_CHARS = ['０', '１', '２', '３', '４', '５', '６', '７', '８', '９'];
+/**
+ * Full Width number characters map.
+ */
+export const DIGIT_Z2H_CHARS_MAP: Record<string, string> = {
+  '０': '0', '１': '1', '２': '2', '３': '3', '４': '4',
+  '５': '5', '６': '6', '７': '7', '８': '8', '９': '9',
+};
 
-export const DIGIT_HANKAKU_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+/**
+ * Half Width DAKUTEN_KANA characters map.
+ */
+export const KANA_H2Z_DAKUTEN_MAP: Record<string, string> = {
+  'ｶ': 'ガ', 'ｷ': 'ギ', 'ｸ': 'グ', 'ｹ': 'ゲ', 'ｺ': 'ゴ',
+  'ｻ': 'ザ', 'ｼ': 'ジ', 'ｽ': 'ズ', 'ｾ': 'ゼ', 'ｿ': 'ゾ',
+  'ﾀ': 'ダ', 'ﾁ': 'ヂ', 'ﾂ': 'ヅ', 'ﾃ': 'デ', 'ﾄ': 'ド',
+  'ﾊ': 'バ', 'ﾋ': 'ビ', 'ﾌ': 'ブ', 'ﾍ': 'ベ', 'ﾎ': 'ボ',
+  'ｳ': 'ヴ', 'ﾞ': 'ﾞ', 'ﾟ': 'ﾟ', // Note: ﾞ and ﾟ are included here as per original Go map
+};
 
-export const KANA_TEN_MAP = new Map([
-  ['ガ', 'ｶ'], ['ギ', 'ｷ'], ['グ', 'ｸ'], ['ゲ', 'ｹ'], ['ゴ', 'ｺ'], ['ザ', 'ｻ'], ['ジ', 'ｼ'], ['ズ', 'ｽ'], ['ゼ', 'ｾ'], ['ゾ', 'ｿ'],
-  ['ダ', 'ﾀ'], ['ヂ', 'ﾁ'], ['ヅ', 'ﾂ'], ['デ', 'ﾃ'], ['ド', 'ﾄ'], ['バ', 'ﾊ'], ['ビ', 'ﾋ'], ['ブ', 'ﾌ'], ['ベ', 'ﾍ'], ['ボ', 'ﾎ'], ['ヴ', 'ｳ']
-]);
+/**
+ * Half Width HANDAKUTEN_KANA characters map.
+ */
+export const KANA_H2Z_MARU_MAP: Record<string, string> = {
+  'ﾊ': 'パ', 'ﾋ': 'ピ', 'ﾌ': 'プ', 'ﾍ': 'ペ', 'ﾎ': 'ポ',
+};
 
-export const KANA_MARU_MAP = new Map([
-  ['パ', 'ﾊ'], ['ピ', 'ﾋ'], ['プ', 'ﾌ'], ['ペ', 'ﾍ'], ['ポ', 'ﾎ']
-]);
+/**
+ * Full Width DAKUTEN_KANA characters map.
+ */
+export const KANA_Z2H_DAKUTEN_MAP: Record<string, string> = {
+  'ガ': 'カ', 'ギ': 'キ', 'グ': 'ク', 'ゲ': 'ケ', 'ゴ': 'コ',
+  'ザ': 'サ', 'ジ': 'シ', 'ズ': 'ス', 'ゼ': 'セ', 'ゾ': 'ソ',
+  'ダ': 'タ', 'ヂ': 'チ', 'ヅ': 'ツ', 'デ': 'テ', 'ド': 'ト',
+  'バ': 'ハ', 'ビ': 'ヒ', 'ブ': 'フ', 'ベ': 'ヘ', 'ボ': 'ホ',
+  'ヴ': 'ウ',
+};
+
+/**
+ * Full Width HANDAKUTEN_KANA characters map.
+ */
+export const KANA_Z2H_MARU_MAP: Record<string, string> = {
+  'パ': 'ハ', 'ピ': 'ヒ', 'プ': 'フ', 'ペ': 'ヘ', 'ポ': 'ホ',
+};
+
+export class ConversionTables {
+  public readonly ASCII_Z2H_CHARS_MAP: Record<string, string>;
+  public readonly ASCII_H2Z_CHARS_MAP: Record<string, string>;
+  public readonly KANA_Z2H_CHARS_MAP: Record<string, string>;
+  public readonly KANA_H2Z_CHARS_MAP: Record<string, string>;
+  public readonly DIGIT_Z2H_CHARS_MAP: Record<string, string>;
+  public readonly DIGIT_H2Z_CHARS_MAP: Record<string, string>;
+  public readonly KANA_Z2H_DAKUTEN_MAP: Record<string, string>;
+  public readonly KANA_Z2H_MARU_MAP: Record<string, string>;
+  public readonly KANA_H2Z_DAKUTEN_MAP: Record<string, string>;
+  public readonly KANA_H2Z_MARU_MAP: Record<string, string>;
+
+  constructor() {
+    this.ASCII_Z2H_CHARS_MAP = ASCII_Z2H_CHARS_MAP;
+    this.ASCII_H2Z_CHARS_MAP = ASCII_H2Z_CHARS_MAP;
+    this.KANA_Z2H_CHARS_MAP = KANA_Z2H_CHARS_MAP;
+    this.KANA_H2Z_CHARS_MAP = KANA_H2Z_CHARS_MAP;
+    this.DIGIT_Z2H_CHARS_MAP = DIGIT_Z2H_CHARS_MAP;
+    this.DIGIT_H2Z_CHARS_MAP = DIGIT_H2Z_CHARS_MAP;
+    this.KANA_Z2H_DAKUTEN_MAP = KANA_Z2H_DAKUTEN_MAP;
+    this.KANA_Z2H_MARU_MAP = KANA_Z2H_MARU_MAP;
+    this.KANA_H2Z_DAKUTEN_MAP = KANA_H2Z_DAKUTEN_MAP;
+    this.KANA_H2Z_MARU_MAP = KANA_H2Z_MARU_MAP;
+  }
+}
+
+/**
+ * Creates and returns a new instance of ConversionTables.
+ * This is equivalent to the New() function in the original Go code.
+ */
+export function createConversionTables(): ConversionTables {
+  return new ConversionTables();
+}
